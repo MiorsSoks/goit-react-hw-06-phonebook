a0b64ab1856bddb3b8c2123605a4efc990e836c2
Myroslav Sokolov <mirossoks@gmail.com>
2022-04-25 19:28:44 +0300

    bagfix contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,25 +1,18 @@
-import { addFilter, getFilter, addItems, getContacts } from 'redux/contactsSlice';
-import { useSelector, useDispatch } from 'react-redux';
+import { getFilter, getContacts } from 'redux/contactsSlice';
+import { useSelector } from 'react-redux';
 import ContactListItem from '../ContactListItem';
 
 export default function ContactList() {
-  const dispatch = useDispatch();
   const filter = useSelector(getFilter);
-  const contacts = useSelector(getContacts)
+  const contacts = useSelector(getContacts);
 
-  const filterContacts = 
-    contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+  const filterContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <ul>
       {filterContacts.map(({ id, name, number }) => (
-        <ContactListItem
-          key={id}
-          id={id}
-          name={name}
-          number={number}
-        />
+        <ContactListItem key={id} id={id} name={name} number={number} />
       ))}
     </ul>
   );
